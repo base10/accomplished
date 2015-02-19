546704c2118a2654ad518b077519b2253788d750
Nathan L. Walls <nathan@wallscorp.us>
2015-02-18 22:46:32 -0500

    Moving more tests from pending to green

--- spec/features/clearance/visitor_resets_password_spec.rb
+++ spec/features/clearance/visitor_resets_password_spec.rb
@@ -2,16 +2,14 @@ require 'spec_helper'
 
 feature 'Visitor resets password' do
   scenario 'by navigating to the page' do
-    pending "Not yet implemented"
     visit sign_in_path
 
     click_link I18n.t('sessions.form.forgot_password')
 
-    current_path.should eq new_password_path
+    expect(current_path).to eq new_password_path
   end
 
   scenario 'with valid email' do
-    pending "Not yet implemented"
     user = user_with_reset_password
 
     page_should_display_change_password_message
@@ -19,7 +17,6 @@ feature 'Visitor resets password' do
   end
 
   scenario 'with non-user account' do
-    pending "Not yet implemented"
     reset_password_for 'unknown.email@example.com'
 
     page_should_display_change_password_message
@@ -29,16 +26,16 @@ feature 'Visitor resets password' do
   private
 
   def reset_notification_should_be_sent_to(user)
-    user.confirmation_token.should_not be_blank
+    expect(user.confirmation_token).not_to be_blank
     mailer_should_have_delivery user.email, 'password', user.confirmation_token
   end
 
   def page_should_display_change_password_message
-    page.should have_content I18n.t('passwords.create.description')
+    expect(page).to have_content I18n.t('passwords.create.description')
   end
 
   def mailer_should_have_delivery(recipient, subject, body)
-    ActionMailer::Base.deliveries.should_not be_empty
+    expect(ActionMailer::Base.deliveries).not_to be_empty
 
     message = ActionMailer::Base.deliveries.any? do |email|
       email.to == [recipient] &&
@@ -46,10 +43,10 @@ feature 'Visitor resets password' do
         email.body =~ /#{body}/
     end
 
-    message.should be
+    expect(message).to be
   end
 
   def mailer_should_have_no_deliveries
-    ActionMailer::Base.deliveries.should be_empty
+    expect(ActionMailer::Base.deliveries).to be_empty
   end
 end
